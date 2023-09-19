ce9c0c69445f0b47664c7c20b5705f4d2f77d387
arisa928 <arisako933@gmail.com>
2023-09-20 00:30:51 +0900

    deviseのviewを変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,12 +30,6 @@ class UsersController < ApplicationController
     @items = @user.items
   end
 
-  def show_favorites
-    @user = User.find(params[:id])
-    @items = item.joins(:likes).where(likes: { user_id: @user.id }).order('likes.created_at DESC')
-
-  end
-
   def edit_profile
     @user = User.find(current_user.id)
   end
