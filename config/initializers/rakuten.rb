354f0e7814053b3d8eaf8ce876de693b1963483c
arisa928 <arisako933@gmail.com>
2023-09-27 23:56:48 +0900

    データベース関係のエラー修正

---
+++ config/initializers/rakuten.rb
@@ -0,0 +1,3 @@
+RakutenWebService.configure do |c|
+  c.application_id = 1056556329200144410
+end
--- config/initializers/rakuten_web_service.rb
+++
@@ -1,4 +0,0 @@
-RakutenWebService.configure do |c|
-  c.application_id = ENV['RAKUTEN_APPLICATION_ID']
-  c.affiliate_id = ENV['RWS_AFFILIATION_ID']
-end
