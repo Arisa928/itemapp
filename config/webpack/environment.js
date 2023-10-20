fc77e4f25e52ec0d66325f639a2bfa397db505ec
arisa928 <arisako933@gmail.com>
2023-10-20 19:48:28 +0900

    ファイル名変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,8 @@
 const { environment } = require('@rails/webpacker')
 
+environment.config.node = {
+  __dirname: true,
+  __filename: true
+}
+
 module.exports = environment
--- webpack.config.js
+++ webpack.config.js
@@ -2,10 +2,23 @@ const path = require('path');
 
 module.exports = {
   entry: {
-    application: '/itemapp/app/javascript/packs/application.js',
+    application: './app/javascript/packs/application.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'), // バンドルされたファイルの出力先ディレクトリ
     filename: 'bundle.js',
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',  // CSSをHTMLファイル内に注入
+          'css-loader',    // CSSファイルを読み込む
+          'sass-loader'    // SCSSをCSSに変換
+        ]
+      }
+    ]
   }
 };
