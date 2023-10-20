e231fd40e50c0c1f2f26402af9e3b1a28cfdfbb4
arisa928 <arisako933@gmail.com>
2023-10-20 15:15:39 +0900

    pathモジュールエラーの修正

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,11 @@
+const path = require('path');
+
 module.exports = {
   entry: {
     application: '/itemapp/app/javascript/packs/application.js',
   },
   output: {
-    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'), // バンドルされたファイルの出力先ディレクトリ
-  },
+    filename: 'bundle.js',
+  }
 };
