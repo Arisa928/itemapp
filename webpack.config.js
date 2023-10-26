4cce6c1792ba6abc9696e8a32496b6ac82fdf000
arisa928 <arisako933@gmail.com>
2023-10-26 14:49:10 +0900

    yarn upgrade --latest使用

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,21 @@ module.exports = {
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader',
-        },
-      },
-    ],
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
   },
   resolve: {
+    // import時の拡張子が不要になる
+    // import SumType from "app/src/sumType.js"の.jsの部分
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
     alias: {
       'channels': path.resolve(__dirname, 'app/javascript/channels/index.js'),
     },
   },
+  // ES5(IE11等)向けの指定
+  target: ["web", "es5"],
 };
