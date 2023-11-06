b311e92cd93f87cf7800603178939cf903437056
arisa928 <arisako933@gmail.com>
2023-11-06 10:33:49 +0900

    Herokuのh10エラーのため設定再確認

--- config/puma.rb
+++ config/puma.rb
@@ -41,7 +41,3 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
-
-# config/puma.rb
-workers 0
-silence_single_worker_warning 
